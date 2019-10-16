b762efccc3ddaa8b231d892a6fe2cbe2aba30875
Treagan Birbal <treaganbirbal@pursuit.org>
2019-10-16 15:57:00 -0400

    started from scatch to count on click

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   // eslint-disable-next-line no-undef
-  const [books, setBooks] = useState({
-    id:0, 
-    title:"",
-    author:"", 
-    submitted:false
-  })
+  const [count, setCount] = React.useState(0)
   return (
     <div className="App">
-      
+      <p>Book ID is {count}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click Me
+        </button>
+
     </div>
   );
 }
