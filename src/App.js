1523e5588607068fb3ce800cf8c7556f3b447245
Treagan Birbal <treaganbirbal@pursuit.org>
2019-10-17 13:30:13 -0400

    implement return in app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 //main compoonent
 function App() {
-  // destructure  value of state, function to update state(like this.setState())
+  // destructure  value of state, function to update state( setTodos like this.setState)
   const [todos, setTodos] = useState([
     {
       text: 'Learn about React', 
@@ -17,6 +17,17 @@ function App() {
       isCompleted: false,
     },
   ])
+
+  return (
+    <div className='app'>
+      <div className='todos-list'>
+        <h1>Todos</h1>
+        {todos.map((todo, index ) => (
+          {/* <Todo /> */}
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App;
